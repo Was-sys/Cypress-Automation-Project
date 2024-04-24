01787ca80b867314e10c254cec3fb0953090a9a7
Was-sys <kumarasinghedarshani78@gmail.com>
2024-04-24 23:45:04 +0530

    added test1

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,5 +1,9 @@
 describe('first test', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('login', () => {
+    cy.visit('https://www.google.com')
+    //cy.get('input[id="user-name"]').type("standard_user");
+    //cy.get('input[id="password"]').type("secret_sauce");
+    //cy.get('input[id="login-button"]').click();
+    
   })
 })
